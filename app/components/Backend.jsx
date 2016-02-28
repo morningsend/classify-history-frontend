55f1dcea26cf702a3988cf660a9b16f2b2f4f8e5
Jake <jh14778.2014@my.bristol.ac.uk>
2016-02-28 23:46:05 +0000

    Exposed more back-end features to the front-end integration.

--- app/components/Backend.jsx
+++ app/components/Backend.jsx
@@ -20,7 +20,41 @@ var Backend = function( backendURL )
         }
       }
     );
-  }
+  };
+
+  this.createTag = function( tagName, onSuccess, onFailure )
+  {
+    $.post( backendURL + "/backend/login", { tagname: tagName },
+      function( data )
+      {
+        if( data.status == "OK" )
+        {
+          onSuccess( data.id );
+        }
+        else
+        {
+          onFailure();
+        }
+      }
+    );
+  };
+
+  this.tagImage = function( imageID, tagID, onSuccess, onFailure )
+  {
+    $.post( backendURL + "/backend/login", { imageid: imageID, tagid: tagID },
+      function( data )
+      {
+        if( data.status == "OK" )
+        {
+          onSuccess();
+        }
+        else
+        {
+          onFailure();
+        }
+      }
+    );
+  };
 };
 
 module.exports = Backend;
